6920e1bb3fae1f3c1252d75a9a561e407510c3ce
Dana Aleksandrova <daleksandrova012@gmail.com>
2025-03-22 13:18:14 +0700

    Delete backend directory

--- backend/products.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'config.php';
-
-$stmt = $pdo->query("SELECT id, name, description, price, image FROM products");
-$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo json_encode($products);
-?>
